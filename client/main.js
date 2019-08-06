2602e681b8f6409857879484665d1f4f922d8f41
Mona-S <forward2cm@gmail.com>
2019-08-06 19:19:56 +0000

    added fav food

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ $(document).ready(startApp);
 
 function startApp(){
     getData();
+    getFood();
     
 }
 
@@ -17,5 +18,21 @@ function getData(){
 }
 
 function handleDataFromServer(response){
+    console.log(response);
+}
+
+function getFood(){
+    var settings = {
+        url: "../server/favfood.json",
+        method: "GET",
+        dataType: "json",
+        success: handleFoodFromServer
+
+    }
+    $.ajax(settings);
+
+}
+
+function handleFoodFromServer(response){
     console.log(response);
 }
